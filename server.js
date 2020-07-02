f6c64f6f0613fadf84812a8f678cfef26d5c13be
Glitch (pyda-info-negara) <none>
2020-07-02 17:35:48 +0000

    🎀💧 Checkpoint ./package.json:12934449/280 ./server.js:12934449/1041 ./public/index.html:12934449/155

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-// server.js
-// where your node app starts
-
-const FORCE_HTTPS = true; // Use this to control the force of HTTPS!
-
-// init project
-const express = require('express');
-const app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+var express = require('express')
+var app = express()
 
 function checkHttps(req, res, next){
   // protocol check, if http, redirect to https
   
   if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+    console.log("https, yo")
     return next()
   } else {
+    console.log("just http")
     res.redirect('https://' + req.hostname + req.url);
   }
 }
 
-// http://expressjs.com/en/starter/static-files.html
+app.all('*', checkHttps)
+app.use(express.static('public'))
+app.listen(process.env.PORT)
