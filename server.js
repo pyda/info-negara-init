190e35f1672a8a5089c420ac586d48a31ffd6b31
Glitch (pyda-info-negara) <none>
2020-07-04 17:29:03 +0000

    🎄💺 Checkpoint ./public/index.html:12934449/35 ./server.js:12934449/1482 ./package.json:12934449/561

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
 
-function checkHttps(req, res, next){
+function checkHttps(req, res, next) {
   // protocol check, if http, redirect to https
-  
-  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
-    console.log("https, yo")
-    return next()
+
+  if (req.get("X-Forwarded-Proto").indexOf("https") != -1) {
+    console.log("https, yo");
+    return next();
   } else {
-    console.log("just http")
-    res.redirect('https://' + req.hostname + req.url);
+    console.log("just http");
+    res.redirect("https://" + req.hostname + req.url);
   }
 }
 
-app.all('*', checkHttps)
-app.use(express.static('public'))
-app.listen(process.env.PORT)
+app.all("*", checkHttps);
+app.use(express.static("public"));
+// Start the server
+app.listen("8000", () => {
+  // eslint-disable-next-line no-console
+  console.log("Local DevServer Started on port 8000...");
+});
